13d13b2cc01d460e0cd072368c945d91f0e7f671
Guglielmo Grillo <guglielmo.grillo@unitn.it>
2025-04-30 17:16:31 +0200

    add support for TOML files

--- computeMSD.c
+++ computeMSD.c
@@ -2,33 +2,60 @@
 #include <stdlib.h>
 
 #include "msd.h"
-//#include "rarray.h"
 #include "parser.h"
 #include "analysis.h"
+#include "configParser.h"
+#include "rarray.h"
 
-#include <math.h>
 
-int main(int argc, char* argv[]) {
+int main(const int argc, char* argv[]) {
     // Let's read filename and output_file from the command line
-    if (argc != 5) {
-        printf("Usage: %s <filename> <output_file_g1> <output_file_g2> <output_file_g3>\n", argv[0]);
+    if (argc != 2) {
+        printf("Usage: %s <filename> <config.ini>\n", argv[0]);
         printf("Input provided:\n");
         for(int a=0; a<argc; a++) {
             printf("\t%s\n", argv[a]);
         }
         return 1;
     }
-    const char* inp_file = argv[1];
-    const char* output_file_g1 = argv[2];
-    const char* output_file_g2 = argv[3];
-    const char* output_file_g3 = argv[4];
+    const char* configfile = argv[1];
 
-    const int64_t T_EQ = 100000000;
+    // READ CONFIGURATION FILE
+    struct config cfg;
+    read_config(configfile, &cfg);
+
+    printf("Config Loaded:\n");
+    printf("\tT_EQ=%ld\n", cfg.T_EQ);
+
+    printf("\tatom_ids=[ ");
+    for(int64_t a=0; a<cfg.atom_ids_size; a++) {printf("%ld ", cfg.atom_ids[a]);}
+    printf("]\n");
+    printf("\tatom_ids_size=%ld\n", cfg.atom_ids_size);
+
+    printf("\tmol_ids=[ ");
+    for(int64_t m=0; m<cfg.mol_ids_size; m++) {printf("%ld ", cfg.mol_ids[m]);}
+    printf("]\n");
+    printf("\tmol_ids_size=%ld\n", cfg.mol_ids_size);
+
+    printf("\tatom_ids=[ ");
+    for(int64_t i=0; i<cfg.atom_types_size; i++) { printf("%ld ", cfg.atom_types[i]); }
+    printf("]\n");
+    printf("\tatom_ids_size=%ld\n", cfg.atom_ids_size);
+
+    printf("\tinput_file=%s\n", cfg.input_file);
+    printf("\tg1_output_file=%s\n", cfg.g1_output_file);
+    printf("\tg2_output_file=%s\n", cfg.g2_output_file);
+    printf("\tg3_output_file=%s\n", cfg.g3_output_file);
+    printf("\tcom_output_file=%s\n", cfg.com_output_file);
+    printf("\n");
+
+
+    // READ LAMMPS DATA
     LAMMPSData data;
 
-    printf("Reading LAMMPS data from %s\n", inp_file);
-    printf("Setting T_EQ = %ld\n", T_EQ);
-    initLAMMPSData(inp_file, &data, T_EQ);
+    printf("Reading LAMMPS data from %s\n", cfg.input_file);
+    printf("Setting T_EQ = %ld\n", cfg.T_EQ);
+    initLAMMPSData(cfg.input_file, &data, cfg.T_EQ);
     checkTimestepMismatch(&data);
 
     printf("\tNumber of atoms: %ld\n", data.num_atoms);
@@ -46,7 +73,7 @@ int main(int argc, char* argv[]) {
     printf("\t%.16e %.16e\n", data.box[4], data.box[5]);
 
     printf("COORDINATES:\n");
-    readLAMMPSCoordinates(inp_file, &data, T_EQ);
+    readLAMMPSCoordinates(cfg.input_file, &data, cfg.T_EQ);
     printf("\tFirst coordinate: %f %f %f\n", data.coordinates[0], data.coordinates[1], data.coordinates[2]);
 
     int64_t offset = (data.num_timesteps-1)*(data.num_atoms)*3 + (data.num_atoms-1)*3;
@@ -74,31 +101,58 @@ int main(int argc, char* argv[]) {
     //                                      177, 181, 209, 210, 211, 212, 216, 217, 245, 246, 282, 288, 318, 319, 355, 356, 390, 431, 432, 456,
     //                                      495, 543, 552, 559, 566, 574, 575, 577, 578, 582, 585, 586};
 
-    // Maxirings for all sims
-    //const int64_t NUMBER_OF_MOLECULES = 3;
-    //int64_t MOLID_RELEVANT_MINIRINGS[3] = { 605, 606, 607 };
+    // EXTRACT THE RELEVANT BEADS FROM THE TRAJECTORY
+    size_t initialCapacity = 1;
+    rarray* beads_buf   = rarray_init(sizeof(int64_t), initialCapacity);
 
-    const int64_t NUMBER_OF_MOLECULES = 1;
-    int64_t MOLID_RELEVANT_MINIRINGS[3] = { 7 };
-
-    int64_t numOfSelectedAtoms;
-    int64_t* sel_atIds = getAtomsFromMoleculeList(&data, MOLID_RELEVANT_MINIRINGS, NUMBER_OF_MOLECULES, &numOfSelectedAtoms);
-    if (numOfSelectedAtoms < 1) {
-        printf("No atom found. Closing.\n");
-        exit(0);
+    // Copy atom id of beads from the config file
+    if (cfg.atom_ids_size > 0) {
+        for(int64_t b=0; b<cfg.atom_ids_size; b++) {
+            rarray_push(beads_buf, cfg.atom_ids+b);
+        }
+    } else {
+        fprintf(stderr, "No bead list provided in the file.\n");
     }
-    printf("I found %ld atoms in the %ld mol specified. I expect %ld=num mol*60 atoms\n", numOfSelectedAtoms, NUMBER_OF_MOLECULES, NUMBER_OF_MOLECULES*60);
 
     /* -------------------------------------------------------------- */
-    //const int64_t TARGET_ATOM_ID = 2;
-    //printf("Extracting the atoms with type %ld.\n", TARGET_ATOM_ID);
-    //int64_t numOfSelectedAtoms;
-    //int64_t* sel_atIds = getAtomsFromType(&data, TARGET_ATOM_ID, &numOfSelectedAtoms);
-    //if (numOfSelectedAtoms < 1) {
-    //    printf("No atom found. Closing.\n");
-    //    exit(0);
-    //}
-    //printf("I found %ld atoms with type %ld\n", numOfSelectedAtoms, TARGET_ATOM_ID);
+    // Select atom_id of beads with molId specified
+    int64_t numOfSelectedAtoms;
+    int64_t* sel_atIds;
+
+    if (cfg.mol_ids_size > 0) {
+        sel_atIds = getAtomsFromMoleculeList(&data, cfg.mol_ids, cfg.mol_ids_size, &numOfSelectedAtoms);
+        if (numOfSelectedAtoms < 1) {
+            printf("No atom found in molids specified in the file %s.\n", cfg.input_file);
+        } else {
+            for(int64_t b=0; b<numOfSelectedAtoms; b++) {
+                rarray_push(beads_buf, sel_atIds+b);
+            }
+        }
+        free(sel_atIds);
+    }
+
+    // Select atom_id of beads with type specified
+    sel_atIds = getAtomsFromType(&data, cfg.atom_types, cfg.atom_types_size, &numOfSelectedAtoms);
+    if (numOfSelectedAtoms < 1) {
+        printf("No atom of type specified in the file %s.\n", cfg.input_file);
+    } else {
+        // Copy atoms
+        for(int64_t b=0; b<numOfSelectedAtoms; b++) {
+            rarray_push(beads_buf, sel_atIds+b);
+        }
+    }
+    free(sel_atIds);
+
+
+    numOfSelectedAtoms =  rarray_size(beads_buf);
+    sel_atIds = (int64_t*) rarray_to_array(beads_buf);
+    rarray_free(beads_buf);
+    if (numOfSelectedAtoms>0) {
+        printf("I selected %ld of %ld atoms.\n", numOfSelectedAtoms, data.num_atoms);
+    } else {
+        fprintf(stderr, "No atom found in the file %s. Closing.\n", cfg.input_file);
+        exit(-1);
+    }
 
 
     /* -------------------------------------------------------------- */
@@ -113,7 +167,6 @@ int main(int argc, char* argv[]) {
     // Average over particles
     for(int64_t p=0; p<numOfSelectedAtoms; p++) {
         // [TODO] Here I am shooting myself in the jewellery. If the code is too slow, this is one point to improve.
-        // [TODO] Here I am shooting myself in the jewellery. If the code is too slow, this is one point to improve.
         double* bead_coord = getBeadTrajectory(&data, sel_atIds[p]);
 
         double* bead_msd   = compute_time_averaged_msd(bead_coord, data.timesteps, data.num_timesteps, data.deltaTimestep);
@@ -127,7 +180,8 @@ int main(int argc, char* argv[]) {
     }
 
     // Let's now print the output to file
-    FILE* output_file = fopen(output_file_g1, "w");
+    FILE* output_file = fopen(cfg.g1_output_file, "w");
+    printf("\t saving to %s\n", cfg.g1_output_file);
     fprintf(output_file, "# TIMESTEP ave_g1\n");
     for (int64_t t=0; t<total_n_windows; t++) {
         fprintf(output_file, "%10ld %12.6f\n", t*data.deltaTimestep, ave_g1[t]);
@@ -148,6 +202,17 @@ int main(int argc, char* argv[]) {
         size_t offset = (size_t) data.num_atoms*t*3;
         compute_CoM(data.coordinates+offset, data.num_atoms, com+3*t);
     }
+
+    output_file = fopen(cfg.com_output_file, "w");
+    printf("\t saving to %s\n", cfg.com_output_file);
+    fprintf(output_file, "# TIMESTEP x y z\n");
+    for (int64_t t=0; t<data.num_timesteps; t++) {
+        double x = com[3*t];
+        double y = com[3*t+1];
+        double z = com[3*t+2];
+        fprintf(output_file, "%10ld %12.6f %12.6f %12.6f\n", t*data.deltaTimestep, x, y, z);
+    }
+    fclose(output_file);
 
 
     /* -------------------------------------------------------------- */
@@ -176,7 +241,8 @@ int main(int argc, char* argv[]) {
     }
 
     // Let's now print the output to file
-    output_file = fopen(output_file_g2, "w");
+    output_file = fopen(cfg.g2_output_file, "w");
+    printf("\t saving to %s\n", cfg.g2_output_file);
     fprintf(output_file, "# TIMESTEP ave_g2\n");
     for (int64_t t=0; t<total_n_windows; t++) {
         fprintf(output_file, "%10ld %12.6f\n", t*data.deltaTimestep, ave_g2[t]);
@@ -194,7 +260,8 @@ int main(int argc, char* argv[]) {
     free(com_msd);
 
     // Let's now print the output to file
-    output_file = fopen(output_file_g3, "w");
+    output_file = fopen(cfg.g3_output_file, "w");
+    printf("\t saving to %s\n", cfg.g3_output_file);
     fprintf(output_file, "# TIMESTEP ave_g3\n");
     for (int64_t t=0; t<total_n_windows; t++) {
         fprintf(output_file, "%10ld %12.6f\n", t*data.deltaTimestep, ave_g3[t]);
@@ -209,6 +276,7 @@ int main(int argc, char* argv[]) {
 
     free(sel_atIds);
     free(com);
+    free_config(&cfg);
 
     return 0;
 }
