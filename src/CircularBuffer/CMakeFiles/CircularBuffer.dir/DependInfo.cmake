9dfddeeea5f2d2c8daae0529960f7ea020a38a58
Guglielmo Grillo <guglielmo.grillo@unitn.it>
2025-11-24 15:56:56 +0100

    move gg_math.c to its own lib

---
+++ src/CircularBuffer/CMakeFiles/CircularBuffer.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/guglielmogrillo/Desktop/jinja-comp-physics-examples/02_In-House_Codes_and_VMC/src/libs/gglib-c/src/CircularBuffer/CircularBuffer.c" "src/libs/gglib-c/src/CircularBuffer/CMakeFiles/CircularBuffer.dir/CircularBuffer.c.o" "gcc" "src/libs/gglib-c/src/CircularBuffer/CMakeFiles/CircularBuffer.dir/CircularBuffer.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CircularBuffer/CMakeFiles/CircularBuffer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CircularBuffer.dir/CircularBuffer.c.o"
+  "CMakeFiles/CircularBuffer.dir/CircularBuffer.c.o.d"
+  "libCircularBuffer.a"
+  "libCircularBuffer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CircularBuffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
